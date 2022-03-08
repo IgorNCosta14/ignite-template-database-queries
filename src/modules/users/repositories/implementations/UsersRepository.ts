cfd892192697a8f2ea2efa64e74a4e85083eaf75
Igor Nascimento costa <igorncosta14@hotmail.com>
2022-03-08 16:31:52 -0300

    08.03

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,31 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne({
+      relations: ["games"],
+      where: { id: user_id }
+    })
+
+    if (!user) throw new Error("User not found");
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      select * 
+      from users
+      order by users.first_name
+    `)
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+    SELECT * FROM users
+    WHERE LOWER(users.first_name) = LOWER($1)
+    AND LOWER(users.last_name) = LOWER($2)
+  `, [first_name, last_name]);
   }
 }
